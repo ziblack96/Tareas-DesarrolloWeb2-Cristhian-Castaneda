3a6ab16ad320c2d32bf00426c635073c54a1de7d
Cristhian Castañeda <casta0124@gmail.com>
2025-10-26 22:51:36 -0600

    practicas con next.js
    making changes for better.

--- cambio_idioma/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Practicas con Next.js</h1>
-    </div>
-  );
-}
